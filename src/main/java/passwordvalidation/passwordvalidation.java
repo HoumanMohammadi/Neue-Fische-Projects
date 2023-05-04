9a435cf900145a6071a2f589da93af82692f9620
Houman Mohammadi <houmane.el@gmail.com>
2023-05-04 11:18:55 +0200

    rename

---
+++ src/main/java/passwordvalidation/passwordvalidation.java
@@ -0,0 +1,5 @@
+package passwordvalidation;
+
+public class passwordvalidation {
+
+}
---
+++ src/test/java/passwordvalidation/passwordvalidationTest.java
@@ -0,0 +1,14 @@
+package passwordvalidation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class passwordvalidationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
